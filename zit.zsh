3c87b36f0e7928e245dd319688a64a45ea73ad0c
Bryce Kwon <signer@brycekwon.dev>
2024-04-15 10:47:40 -0700

    Update project for personal use

--- zit.zsh
+++ zit.zsh
@@ -1,128 +1,77 @@
-# Zit - minimal plugin manager for ZSH
+################################################################################
+#                           Storing Environment Variables                      #
+################################################################################
 
-# store all loaded modules to paths
-export -Ua ZIT_MODULES_LOADED
-
-# store all modules to be upgraded
-export -Ua ZIT_MODULES_UPGRADE
-
-# set variable below to change zit modules path
-if [[ -z "${ZIT_MODULES_PATH}" ]]; then
-  export ZIT_MODULES_PATH="${ZDOTDIR:-${HOME}}"
+# define path where zit modules will be stored
+if [[ -z "${ZIT_MODULES_HOME}" ]]; then
+    export ZIT_MODULES_HOME="${ZDOTDIR:-${HOME}}"
 fi
 
-_zit-param-validation() {
-  local name="${1}"
-  local param="${2}"
 
-  if [[ -z "${param}" ]]; then
-    printf '[zit] Missing argument: %s\n' "${name}"
-    return 1
-  fi
-  return 0
+################################################################################
+#                              Define Helper Functions                         #
+################################################################################
+
+# verify parameters on function call
+function __zit-parameter-validation() {
+    if [[ -z "${2}" ]]; then
+        printf "[zit] missing arguments: %s\n" "${1}"
+        return 1
+    fi
+    return 0
 }
 
-# loader
-zit-load() {
-  _zit-param-validation 'Module directory' "${1}" || return 1
-  _zit-param-validation '.zsh file' "${2}" || return 2
 
-  local module_dir="${ZIT_MODULES_PATH}/${1}"
-  local dot_zsh="${2}"
+################################################################################
+#                              Define Primary Functions                        #
+################################################################################
 
-  # shellcheck source=/dev/null
-  source "${module_dir}/${dot_zsh}" || return 255
-  # added to global dir array for tracking
-  ZIT_MODULES_LOADED+=("${module_dir}")
+# install plugin from url in format 'http(s)://<URL>#<BRANCH>'
+function zit-install() {
+    # verify function parameters
+    __zit-parameter-validation "plugin name" "${1}" || return 1
+    __zit-parameter-validation "repository url" "${2}" || return 1
+
+    # parse git url and branch
+    local git_repo="${2%%#*}"
+    local git_branch="${2#*#}" && [[ $git_branch != $2 ]] || git_branch=""
+    if [[ -z "${git_repo}" ]]; then
+        printf "[zit] invalid url: 'http(s)://<URL>#<BRANCH>'\n" 
+        return 1
+    fi
+
+    # clone git repository and specified branch (if applicable)
+    local module_dir="${ZIT_MODULES_HOME}/${1}"
+    if [[ ! -d "${module_dir}" ]]; then
+        printf "[zit] installing: %s\n" "${module_dir}"
+        if [[ -z "${git_branch}" ]]; then
+            git clone --recurse-submodules --depth 1 \
+                "${git_repo}" "${module_dir}" > /dev/null
+        else
+            git clone --recurse-submodules --depth 1 \
+                "${git_repo}" -b "${git_branch}" "${module_dir}" > /dev/null
+        fi
+    fi
 }
 
-# installer
-zit-install() {
-  _zit-param-validation 'Git repo' "${1}" || return 1
-  _zit-param-validation 'Module directory' "${2}" || return 2
+# load plugin from module directory
+function zit-load() {
+    __zit-parameter-validation "plugin name" "${1}" || return 1
+    __zit-parameter-validation "source file" "${2}" || return 1
 
-  local git_url="${1}"
-  local module_dir="${ZIT_MODULES_PATH}/${2}"
-
-  # http://zsh.sourceforge.net/Doc/Release/Expansion.html#Parameter-Expansion
-  # https://github.com/thiagokokada/zit#branch -> https://github.com/thiagokokada/zit
-  local git_repo="${git_url%'#'*}"
-
-  local git_branch="${git_url#*'#'}"
-  # https://github.com/thiagokokada/zit -> master
-  # https://github.com/thiagokokada/zit# -> master
-  # https://github.com/thiagokokada/zit#branch -> branch
-  if [[ -z "${git_branch}" ]] || [[ "${git_branch}" == "${git_url}" ]]; then
-    git_branch="master"
-  fi
-
-  # clone module
-  if [[ ! -d "${module_dir}" ]]; then
-    printf 'Installing %s\n' "${module_dir}"
-    git clone --recurse-submodules \
-      "${git_repo}" -b "${git_branch}" "${module_dir}"
-    printf '\n'
-  fi
-  # added to global dir array for updater
-  [[ -z "${ZIT_DISABLE_UPGRADE}" ]] && ZIT_MODULES_UPGRADE+=("${module_dir}")
+    # source the main function for the plugin
+    source "${ZIT_MODULES_HOME}/${1}/${2}" || return 1
 }
 
-# do both above in one step
-zit-install-load() {
-  _zit-param-validation 'Git repo' "${1}" || return 1
-  _zit-param-validation 'Module directory' "${2}" || return 2
-  _zit-param-validation '.zsh file' "${3}" || return 3
+# update plugin from remote url
+function zit-upgrade() {
+    # pull new changes into each module repository
+    for i in $(command ls $ZIT_MODULES_HOME); do
+        pushd "$ZIT_MODULES_HOME/${i}" > /dev/null || continue
 
-  local git_repo="${1}"
-  local module_dir="${2}"
-  local dot_zsh="${3}"
+        printf "[zit] updating: %s\n" "${i}"
+        git pull > /dev/null
 
-  zit-install "${git_repo}" "${module_dir}"
-  zit-load "${module_dir}" "${dot_zsh}"
+        popd > /dev/null || continue
+    done
 }
-
-# upgrade modules
-zit-upgrade() {
-  local module_dir
-
-  for module_dir in "${ZIT_MODULES_UPGRADE[@]}"; do
-    pushd "${module_dir}" > /dev/null || continue
-    printf 'Updating %s\n' "${module_dir}"
-    git pull
-    printf '\n'
-    popd > /dev/null || continue
-  done
-}
-alias zit-update='zit-upgrade' # deprecated
-
-# remove module
-zit-remove() {
-  _zit-param-validation 'Module directory' "${1}" || return 1
-
-  local module_dir="${1}"
-  local module_path="${ZIT_MODULES_PATH}/${module_dir}"
-
-  if [[ -z "${ZIT_MODULES_UPGRADE[(r)${module_path}]}" ]]; then
-    printf 'No module %s is installed.\n' "${module_dir}"
-    return 255
-  fi
-
-  while true; do
-    printf 'Really delete module %s? (y/n) ' "${module_dir}"
-    read -r yn
-    case $yn in
-      [Yy]* ) rm -rf "${ZIT_MODULES_PATH:?}/${module_dir}"
-              break;;
-      [Nn]* ) break;;
-      * ) echo 'Please answer (y)es or (n)o.';;
-    esac
-  done
-}
-
-alias zit-in='zit-install'
-alias zit-lo='zit-load'
-alias zit-il='zit-install-load'
-alias zit-up='zit-upgrade'
-alias zit-rm='zit-remove'
-
-# vim: ft=zsh
